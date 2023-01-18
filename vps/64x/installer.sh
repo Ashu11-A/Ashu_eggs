9b634325f12779ae35aa3e6240da62695c521a3c
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-01-18 16:36:05 -0400

    atualizar dependencias

--- vps/64x/installer.sh
+++ vps/64x/installer.sh
@@ -34,11 +34,12 @@ ${bold}${lightblue} :           :     : :: ::    :   : :   : :  :         :     
                                                                                                   
                                                                                                                 
 ${bold}${lightgreen}========================================================================
+${bold}${lightblue}       ...ISTO PODE DEMORAR MAIS DE 15 MINUTOS SEJA PACIENTE...
  "
  
 echo "${nc}"
 
-if [[ -f "./installed" ]]; then
+if [[ -f "./instalado" ]]; then
     echo "${bold}${lightgreen}==> Started ${lightblue}Container${lightgreen} <=="
     function runcmd1 {
         printf "${bold}${lightgreen}Default${nc}@${lightblue}Container${nc}:~ "
@@ -55,33 +56,33 @@ if [[ -f "./installed" ]]; then
     runcmd
 else
     echo "Downloading files for application"
-    curl -sSLo ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip >/dev/null 2>err.log
+    curl -sSLo ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz >/dev/null 2>err.log
     echo -ne '#                   (5%)\r'
-    curl -sSLo files.zip https://github.com/RealTriassic/Ptero-VM-JAR/releases/download/latest/files.zip >/dev/null 2>err.log
+    mkdir libraries
+    curl -sSLo ./libraries/proot https://github.com/proot-me/proot/releases/download/v5.3.0/proot-v5.3.0-x86_64-static >/dev/null 2>err.log
     echo -ne '##                  (10%)\r'
-    curl -sSLo unzip https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/unzip >/dev/null 2>err.log
+    curl -sSLo root.tar.xz https://github.com/termux/proot-distro/releases/download/v3.3.0/debian-x86_64-pd-v3.3.0.tar.xz >/dev/null 2>err.log
     echo -ne '####                (20%)\r'
-    curl -sSLo gotty https://raw.githubusercontent.com/afnan007a/Replit-Vm/main/gotty >/dev/null 2>err.log
+    curl -sSLo gotty.tar.gz https://github.com/sorenisanerd/gotty/releases/download/v1.5.0/gotty_v1.5.0_linux_amd64.tar.gz >/dev/null 2>err.log
     echo -ne '#####               (25%)\r'
-    chmod +x unzip >/dev/null 2>err.log
     export PATH="/bin:/usr/bin:/usr/local/bin:/sbin:$HOMEA/bin:$HOMEA/usr/bin:$HOMEA/sbin:$HOMEA/usr/sbin:$HOMEA/etc/init.d:$PATH"
-    ./unzip ngrok.zip >/dev/null 2>err.log
     echo -ne '######               (30%)\r'
-    ./unzip files.zip >/dev/null 2>err.log
+    tar -xvf root.tar.xz >/dev/null 2>err.log
     echo -ne '#######              (35%)\r'
-    ./unzip root.zip
-    tar -xf root.tar.gz >/dev/null 2>err.log
-    echo -ne '########             (40%)\r'
     chmod +x ./libraries/proot >/dev/null 2>err.log
+    echo -ne '########             (40%)\r'
+    tar -cvzf ngrok.tgz >/dev/null 2>err.log
     echo -ne '#########            (45%)\r'
     chmod +x ngrok >/dev/null 2>err.log
     echo -ne '##########           (50%)\r'
+    tar -xzvf gotty.tar.gz
     chmod +x gotty >/dev/null 2>err.log
     echo -ne '###########          (55%)\r'
     rm -rf files.zip >/dev/null 2>err.log
     rm -rf root.zip >/dev/null 2>err.log
-    rm -rf root.tar.gz >/dev/null 2>err.log
-    rm -rf ngrok.zip >/dev/null 2>err.log
+    rm -rf root.tar.xz >/dev/null 2>err.log
+    rm -rf gotty.tar.gz >/dev/null 2>err.log
+    rm -rf ngrok.tgz >/dev/null 2>err.log
     echo -ne '############         (60%)\r'
 
     cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "mv ngrok /usr/bin/" "apt-get update" "apt-get -y upgrade" "apt-get -y install sudo curl wget hwloc htop nano neofetch python3" "curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py")
@@ -91,7 +92,7 @@ else
     done
     echo -ne '####################(100%)\r'
     echo -ne '\n'
-    touch installed
+    touch instalado
     
     echo "
 ${bold}${lightgreen}========================================================================
