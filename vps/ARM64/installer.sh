4c6e674c9940b73fef3fff16210cf531e4baacbe
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-01-18 17:23:29 -0400

    Update installer.sh

--- vps/ARM64/installer.sh
+++ vps/ARM64/installer.sh
@@ -88,7 +88,7 @@ else
     cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "mv ngrok /usr/bin/" "apt-get update" "apt-get -y upgrade" "apt-get -y install sudo curl wget hwloc htop nano neofetch python3" "curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py")
 
     for cmd in "${cmds[@]}"; do
-        ./libraries/proot -S . /bin/bash -c "$cmd -q qemu-arm >/dev/null 2>err.log"
+        ./libraries/proot -S . /bin/bash -c "$cmd -q qemu-arm64 >/dev/null 2>err.log"
     done
     echo -ne '####################(100%)\r'
     echo -ne '\n'
