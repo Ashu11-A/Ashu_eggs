8f21979caea609433db06cd075d3ae70b455b609
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-01-19 09:17:13 -0400

    Remover coisas inúteis do Selector

--- vps/selector.sh
+++ vps/selector.sh
@@ -6,8 +6,6 @@ ARCH=$([ "$(uname -m)" == "x86_64" ] && echo "amd64" || echo "arm64")
 if [ "${ARCH}" == "arm64" ];
 then
 bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/vps/ARM64/installer.sh)
-
 else 
-echo "${bold}${lightblue}         ...Arquitetura x86_64 detectada..."
 bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/vps/64x/installer.sh)
 fi
