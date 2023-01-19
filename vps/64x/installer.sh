c58572d8fa403e49014e1f91e1e4812b49e1a932
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-01-19 09:16:37 -0400

    Add Selector

--- vps/64x/installer.sh
+++ vps/64x/installer.sh
@@ -54,6 +54,7 @@ if [[ -f "./instalado" ]]; then
     }
     runcmd
 else
+    echo "${bold}${lightblue}                    ...Arquitetura x86_64 detectada..."
     echo "${bold}${lightblue}          ...ISTO PODE DEMORAR MAIS DE 15 MINUTOS SEJA PACIENTE..."
     echo "Baixando arquivos para iniciar a vps"
     curl -sSLo ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz >/dev/null 2>err.log
--- vps/selector.sh
+++ vps/selector.sh
@@ -1,6 +1,13 @@
-ARCH=$([ "$(uname -m)" == "x86_64" ] && 
-echo "amd64" || echo "arm64") && 
-if [ "${ARCH}" == "arm64" ]; 
-then bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/vps/ARM64/installer.sh)
-else bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/vps/64x/installer.sh)
+bold=$(echo -en "\e[1m")
+lightblue=$(echo -en "\e[94m")
+
+ARCH=$([ "$(uname -m)" == "x86_64" ] && echo "amd64" || echo "arm64")
+
+if [ "${ARCH}" == "arm64" ];
+then
+bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/vps/ARM64/installer.sh)
+
+else 
+echo "${bold}${lightblue}         ...Arquitetura x86_64 detectada..."
+bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/vps/64x/installer.sh)
 fi
