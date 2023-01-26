9976b2281d0c178d15bbf6483dbfbd4ee0f10bad
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-01-25 23:54:53 -0400

    Update start.sh

--- Frp/start.sh
+++ Frp/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ "${FRP_MODE}" == "1" ]; then
-    echo "⚙️ Versão do Script: 1.6"
+    echo "⚙️ Versão do Script: 1.7"
     echo "👀 Saiba Mais: https://github.com/fatedier/frp"
     echo "✅ Iniciando Frps"
     cp -f ./Frpc/frpc.ini ./exemplo_frpc_windows64/frpc.ini
     ./Frps/frps -c ./Frps/frps.ini
 else
-    echo "⚙️ Versão do Script: 1.6"
+    echo "⚙️ Versão do Script: 1.7"
     echo "👀 Saiba Mais: https://github.com/fatedier/frp"
     echo "✅ Iniciando Frpc"
     cp -f ./Frpc/frpc.ini ./exemplo_frpc_windows64/frpc.ini
