73beec2d86a22afa0dc8e2fdc4c54aa832fb3643
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-01-26 14:24:20 -0400

    Atualizar script de Inicialização

--- MTA/start.sh
+++ MTA/start.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-if [[ -f "./mta-server64" ]]; then
-    echo "⚙️ Versão do Script: 1.2"
-    echo "✅ Iniciando MTA"
-    ./mta-server64 -n
+ARCH=$([ "$(uname -m)" == "x86_64" ] && echo "amd64" || echo "arm64")
+
+if [ "${ARCH}" == "amd64" ];
+then
+    echo "🔎 Arquitetura Identificada: 64x"
+    if [[ -f "./mta-server64" ]]; then
+        echo "⚙️ Versão do Script: 1.3"
+        echo "✅ Iniciando MTA"
+        ./mta-server64 -n
+    else
+        echo "MTA Não Instalado, isso é realmente muito estranho, essa é uma segunda verificação."
+    fi
 else
-    echo "MTA Não Instalado, isso é realmente muito estranho, essa é uma segunda verificação."
+    echo "🔎 Arquitetura Identificada: ARM64"
+    echo "⚠️ Atenção: Este Egg ainda não funciona no ARM64"
+    if [[ -f "./mta-server64" ]]; then
+        echo "⚙️ Versão do Script: 1.3"
+        echo "✅ Iniciando MTA"
+        ./mta-server64 -n
+    else
+        echo "MTA Não Instalado, isso é realmente muito estranho, essa é uma segunda verificação."
+    fi
 fi
