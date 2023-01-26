ae1f25e2bca06d8c0ce862cd60cd318fb5f3b87a
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-01-26 00:02:02 -0400

    Corrigir pasta

--- Frp/start.sh
+++ Frp/start.sh
@@ -3,12 +3,12 @@ if [ "${FRP_MODE}" == "1" ]; then
     echo "⚙️ Versão do Script: 1.7"
     echo "👀 Saiba Mais: https://github.com/fatedier/frp"
     echo "✅ Iniciando Frps"
-    cp -f ./Frpc/frpc.ini ./exemplo_frpc_windows64/frpc.ini
+    cp -f ./Frpc/frpc.ini ./Exemplo_Frpc_Windows64/frpc.ini
     ./Frps/frps -c ./Frps/frps.ini
 else
     echo "⚙️ Versão do Script: 1.7"
     echo "👀 Saiba Mais: https://github.com/fatedier/frp"
     echo "✅ Iniciando Frpc"
-    cp -f ./Frpc/frpc.ini ./exemplo_frpc_windows64/frpc.ini
+    cp -f ./Frpc/frpc.ini ./Exemplo_Frpc_Windows64/frpc.ini
     ./Frpc/frpc -c ./Frpc/frpc.ini
 fi
