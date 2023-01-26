0811a27e7e2f7de4e85bfb2e53fc835fc4579e91
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-01-26 10:35:54 -0400

    Correção de sintase

--- Jellyfin/install.sh
+++ Jellyfin/install.sh
@@ -3,22 +3,22 @@ if [[ -f "./jellyfin/jellyfin.dll" ]]; then
     bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Jellyfin/start.sh)
 else
 
-mkdir -p /mnt/server
-cd /mnt/server
-DOWNLOAD_LINK=$(echo https://repo.jellyfin.org/releases/server/portable/versions/stable/combined/${VERSION}/jellyfin_${VERSION}.tar.gz)
-cat <<EOF > .log.txt
+    mkdir -p /mnt/server
+    cd /mnt/server
+    DOWNLOAD_LINK=$(echo https://repo.jellyfin.org/releases/server/portable/versions/stable/combined/${VERSION}/jellyfin_${VERSION}.tar.gz)
+    cat <<EOF > .log.txt
 Versão: ${VERSION}
 Link: ${DOWNLOAD_LINK}
 Arquivo: ${DOWNLOAD_LINK##*/}
 EOF
-rm -rf *
-curl -sSL ${DOWNLOAD_LINK} -o ${DOWNLOAD_LINK##*/}
-tar -vzxf ${DOWNLOAD_LINK##*/}
-mkdir jellyfin
-mv jellyfin_${VERSION}/* ./jellyfin/
-mkdir .config
-mkdir .config/jellyfin
-cat <<EOF > .config/jellyfin/network.xml
+    rm -rf *
+    curl -sSL ${DOWNLOAD_LINK} -o ${DOWNLOAD_LINK##*/}
+    tar -vzxf ${DOWNLOAD_LINK##*/}
+    mkdir jellyfin
+    mv jellyfin_${VERSION}/* ./jellyfin/
+    mkdir .config
+    mkdir .config/jellyfin
+    cat <<EOF > .config/jellyfin/network.xml
 <?xml version="1.0" encoding="utf-8"?>
 <NetworkConfiguration xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
 <RequireHttps>false</RequireHttps>
@@ -45,7 +45,7 @@ cat <<EOF > .config/jellyfin/network.xml
 <KnownProxies />
 </NetworkConfiguration>
 EOF
-rm -rf ${DOWNLOAD_LINK##*/}
-rm -rf jellyfin_${VERSION}
-echo "user_allow_other" >> /etc/fuse.conf
+    rm -rf ${DOWNLOAD_LINK##*/}
+    rm -rf jellyfin_${VERSION}
+    echo "user_allow_other" >> /etc/fuse.conf
 fi
