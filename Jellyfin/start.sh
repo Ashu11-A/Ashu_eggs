d18f31cafd9d76a1d77854a85c38e4ee88f4f0b2
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-01-25 21:00:55 -0400

    Mudança da forma de Instalação

---
+++ Jellyfin/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [[ -f "./jellyfin/jellyfin.dll" ]]; then
+    echo "✓ Iniciando Jellyfin"
+    dotnet jellyfin/jellyfin.dll --ffmpeg /usr/lib/jellyfin-ffmpeg/ffmpeg
+else
+    echo "Painel Não Instalado, isso é realmente muito estranho, essa é uma segunda verificação."
+fi
