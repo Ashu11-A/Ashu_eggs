9365b40c97fc98a35ca2eadc30a280d3ce1386d6
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-01-26 00:05:35 -0400

    Criação do Ex

--- Frp/start.sh
+++ Frp/start.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+if [ "${INSTALL_EX}" == "1" ]; then
+    cp -f ./Frpc/frpc.ini ./Exemplo_Frpc_Windows64/frpc.ini
+else
+    echo""
+fi
+
 if [ "${FRP_MODE}" == "1" ]; then
     echo "⚙️ Versão do Script: 1.7"
     echo "👀 Saiba Mais: https://github.com/fatedier/frp"
     echo "✅ Iniciando Frps"
-    cp -f ./Frpc/frpc.ini ./Exemplo_Frpc_Windows64/frpc.ini
     ./Frps/frps -c ./Frps/frps.ini
 else
     echo "⚙️ Versão do Script: 1.7"
     echo "👀 Saiba Mais: https://github.com/fatedier/frp"
     echo "✅ Iniciando Frpc"
-    cp -f ./Frpc/frpc.ini ./Exemplo_Frpc_Windows64/frpc.ini
     ./Frpc/frpc -c ./Frpc/frpc.ini
 fi
