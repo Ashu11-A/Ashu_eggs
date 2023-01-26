3cc67786e106aa4e49d4c682be697550c5bdcb3d
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-01-26 16:46:55 -0400

    Correções

--- OpenVPN/install.sh
+++ OpenVPN/install.sh
@@ -2,6 +2,7 @@
 if [[ -f "./Logs/instalado" ]]; then
     bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/OpenVPN/start.sh)
 else
+    mkdir Logs
     export AUTO_INSTALL=y
     export APPROVE_INSTALL=y
     export APPROVE_IP=y
@@ -13,7 +14,6 @@ else
     export CUSTOMIZE_ENC=n
     export CLIENT=clientname
     export PASS=1
-    bash <(curl -s https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh)
-    mkdir Logs
+    bash <(curl -s https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh) >/dev/null 2>Logs/err.log
     touch ./Logs/instalado
 fi
