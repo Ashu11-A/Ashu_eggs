e5d7797c700fbc8437e23ab1a80e1ee582fa65dd
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-01-26 10:20:54 -0400

    Script do Emby

---
+++ Emby/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [[ -f "./Emby/EmbyServer.dll" ]]; then
+    echo "⚙️ Versão do Script: 1.0"
+    echo "✅ Iniciando Emby"
+    dotnet Emby/EmbyServer.dll -ffmpeg /usr/bin/ffmpeg -ffprobe /usr/bin/ffprobe
+else
+    echo "Painel Não Instalado, isso é realmente muito estranho, essa é uma segunda verificação."
+fi
