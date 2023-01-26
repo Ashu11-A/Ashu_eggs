7f51a16f703e2fb2b297257c63d15041eb1e06f0
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-01-25 21:44:20 -0400

    Adicionar Versão do Script

--- Jellyfin/start.sh
+++ Jellyfin/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [[ -f "./jellyfin/jellyfin.dll" ]]; then
+    echo "Versão do Script: 1.0"
     echo "✓ Iniciando Jellyfin"
     dotnet jellyfin/jellyfin.dll --ffmpeg /usr/lib/jellyfin-ffmpeg/ffmpeg
 else
