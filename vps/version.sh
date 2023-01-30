b4731b29731d9f2697984e65a9e52a0017b2cc80
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-01-30 17:16:07 -0400

    Correções

--- vps/version.sh
+++ vps/version.sh
@@ -1,11 +1,14 @@
+version_egg="2.0"
+version_script="2.1"
+
 if [[ -f "./libraries/version" ]]; then
     versions=" $(cat ./libraries/version) " 
     comm1=$( printf '%s\n' "$versions" | tr -d '.' )
     comm2=$( printf '%s\n' "$version_egg" | tr -d '.' )
     if [[ -f "./libraries/version_system" ]]; then
         version_system=" $(cat ./libraries/version_system) " 
-        if [ $version_system = "true" ]; then
-            if [ $comm1 -ge $comm2  ]; then
+        if [ "$version_system" = "true" ]; then
+            if [ "$comm1" -ge "$comm2"  ]; then
                 echo "✅  Egg Atualizado."
             else
                 echo "
