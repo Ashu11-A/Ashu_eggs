9c196bf567ad503b319d6f9a14410d4a4ffabdd9
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-02-03 01:56:47 -0400

    retirar break

--- Paneldactyl/cron.sh
+++ Paneldactyl/cron.sh
@@ -4,7 +4,7 @@ do
   if [ "${PANEL}" = "Jexactyl" ] || [ "${PANEL}" = "Jexactyl Brasil" ]; then
     HORA_CERTA='0000'
     HORA=$(date +%H%M) # captura a hora
-    [ "$HORA" == "$HORA_CERTA" ] && php /var/www/jexactyl/artisan p:schedule:renewal >> /dev/null 2>&1 && break # executa o script desejado
+    [ "$HORA" == "$HORA_CERTA" ] && php /var/www/jexactyl/artisan p:schedule:renewal >> /dev/null 2>&1 # executa o script desejado
   fi
   
   php /home/container/painel/artisan schedule:run >> /dev/null 2>&1 && /bin/bash /crontab_test.sh
