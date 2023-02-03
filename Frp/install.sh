e6999b4d345386e813f133deb7f901cd843726ab
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-02-02 22:16:41 -0400

    correções de sintase

--- Frp/install.sh
+++ Frp/install.sh
@@ -3,7 +3,7 @@ if [[ -f "./Frps/frps" ]]; then
     bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Frp/start.sh)
 else
 
-    cd /mnt/server
+    cd /mnt/server || exit
 
     GITHUB_PACKAGE=fatedier/frp
     LATEST_JSON=$(curl --silent "https://api.github.com/repos/$GITHUB_PACKAGE/releases/latest" | jq -c '.[]' | head -1)
@@ -13,27 +13,27 @@ else
     if [ "${ARCH}" == "arm64" ]; then
         if [ -z "$VERSION" ] || [ "$VERSION" == "latest" ]; then
             echo -e "defaulting to latest release"
-            DOWNLOAD_LINK=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i linux | grep -i arm64)
+            DOWNLOAD_LINK=$(echo "$LATEST_JSON" | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i linux | grep -i arm64)
         else
-            VERSION_CHECK=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .tag_name')
+            VERSION_CHECK=$(echo "$RELEASES" | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .tag_name')
         if [ "$VERSION" == "$VERSION_CHECK" ]; then
-            DOWNLOAD_LINK=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .assets[].browser_download_url' | grep -i frp | grep -i linux | grep -i arm64)
+            DOWNLOAD_LINK=$(echo "$RELEASES" | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .assets[].browser_download_url' | grep -i frp | grep -i linux | grep -i arm64)
         else
             echo -e "defaulting to latest release"
-            DOWNLOAD_LINK=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i linux | grep -i arm64)
+            DOWNLOAD_LINK=$(echo "$LATEST_JSON" | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i linux | grep -i arm64)
         fi
         fi
         else
             if [ -z "$VERSION" ] || [ "$VERSION" == "latest" ]; then
                 echo -e "defaulting to latest release"
-                DOWNLOAD_LINK=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i linux | grep -i amd64)
+                DOWNLOAD_LINK=$(echo "$LATEST_JSON" | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i linux | grep -i amd64)
             else
-                VERSION_CHECK=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .tag_name')
+                VERSION_CHECK=$(echo "$RELEASES" | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .tag_name')
             if [ "$VERSION" == "$VERSION_CHECK" ]; then
-                DOWNLOAD_LINK=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .assets[].browser_download_url' | grep -i frp | grep -i linux | grep -i amd64)
+                DOWNLOAD_LINK=$(echo "$RELEASES" | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .assets[].browser_download_url' | grep -i frp | grep -i linux | grep -i amd64)
             else
                 echo -e "defaulting to latest release"
-                DOWNLOAD_LINK=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i linux | grep -i amd64)
+                DOWNLOAD_LINK=$(echo "$LATEST_JSON" | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i linux | grep -i amd64)
             fi
         fi
     fi
@@ -53,12 +53,12 @@ Arquivo: ${DOWNLOAD_LINK##*/}
 EOF
 
     echo -e "running 'curl -sSL ${DOWNLOAD_LINK} -o ${DOWNLOAD_LINK##*/}'"
-    curl -sSL ${DOWNLOAD_LINK} -o ${DOWNLOAD_LINK##*/}
+    curl -sSL "${DOWNLOAD_LINK}" -o "${DOWNLOAD_LINK##*/}"
     echo -e "Unpacking server files"
-    tar -xvzf ${DOWNLOAD_LINK##*/}
+    tar -xvzf "${DOWNLOAD_LINK##*/}"
     cp -R frp*/* ./
     rm -rf frp*linux*
-    rm -rf ${DOWNLOAD_LINK##*/}
+    rm -rf "${DOWNLOAD_LINK##*/}"
 
     cat <<EOF > frpc.ini
 [common]
@@ -109,17 +109,17 @@ EOF
     if [ "${INSTALL_EX}" == "1" ]; then
         mkdir Exemplo_Frpc_Windows64
         cp -f ./Frpc/frpc.ini ./Exemplo_Frpc_Windows64/frpc.ini
-        cd Exemplo_Frpc_Windows64
+        cd Exemplo_Frpc_Windows64 || exit
             if [ -z "$VERSION" ] || [ "$VERSION" == "latest" ]; then
                 echo -e "defaulting to latest release"
-                DOWNLOAD_LINK_EX=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i windows | grep -i amd64)
+                DOWNLOAD_LINK_EX=$(echo "$LATEST_JSON" | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i windows | grep -i amd64)
             else
-                VERSION_CHECK=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .tag_name')
+                VERSION_CHECK=$(echo "$RELEASES" | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .tag_name')
             if [ "$VERSION" == "$VERSION_CHECK" ]; then
-                DOWNLOAD_LINK_EX=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .assets[].browser_download_url' | grep -i frp | grep -i windows | grep -i amd64)
+                DOWNLOAD_LINK_EX=$(echo "$RELEASES" | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .assets[].browser_download_url' | grep -i frp | grep -i windows | grep -i amd64)
             else
                 echo -e "defaulting to latest release"
-                DOWNLOAD_LINK_EX=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i windows | grep -i amd64)
+                DOWNLOAD_LINK_EX=$(echo "$LATEST_JSON" | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i windows | grep -i amd64)
             fi
         fi
 
@@ -130,12 +130,12 @@ Arquivo: ${DOWNLOAD_LINK_EX##*/}
 EOF
 
         echo -e "running 'curl -sSL ${DOWNLOAD_LINK_EX} -o ${DOWNLOAD_LINK_EX##*/}'"
-        curl -sSL ${DOWNLOAD_LINK_EX} -o ${DOWNLOAD_LINK_EX##*/}
+        curl -sSL "${DOWNLOAD_LINK_EX}" -o "${DOWNLOAD_LINK_EX##*/}"
         echo -e "Unpacking server files"
-        unzip ${DOWNLOAD_LINK_EX##*/}
+        unzip "${DOWNLOAD_LINK_EX##*/}"
         cp -R frp*/* ./
         rm -rf frp*windows*
-        rm -rf ${DOWNLOAD_LINK_EX##*/}
+        rm -rf "${DOWNLOAD_LINK_EX##*/}"
         rm frps*
         rm LICENSE
         cat <<EOF > start.bat
