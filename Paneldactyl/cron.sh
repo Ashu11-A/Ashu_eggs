fec73470586784121b82d3d8d20d94dcea1b1ba8
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-02-03 01:38:47 -0400

    correção

--- Paneldactyl/cron.sh
+++ Paneldactyl/cron.sh
@@ -10,7 +10,7 @@ if [ "${PANEL}" = "Jexactyl" ] || [ "${PANEL}" = "Jexactyl Brasil" ]; then
     while :
     do
     HORA=$(date +%H%M) # captura a hora
-    [ "$HORA" == "$HORA_CERTA" ] && executa_script.sh && break# executa o script desejado
+    [ "$HORA" == "$HORA_CERTA" ] && php /var/www/jexactyl/artisan p:schedule:renewal >> /dev/null 2>&1 && break # executa o script desejado
     sleep 60 # espera um minuto pra tentar verificar novamente
     done
 fi
