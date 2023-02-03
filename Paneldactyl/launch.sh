e7df94b6893f9a1eae5490094a63e13f3df4629c
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-02-02 22:37:31 -0400

    correções

--- Paneldactyl/launch.sh
+++ Paneldactyl/launch.sh
@@ -14,7 +14,8 @@ echo "🟢  Iniciando PHP-FPM..."
 /usr/sbin/php-fpm81 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo "🟢  Iniciando Nginx..."
+touch ./nohup.out
 nohup /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/ &
 echo "🟢 Iniciando worker do painel"
-php /home/container/painel/artisan queue:work --queue=high,standard,low --sleep=3 --tries=3
+nohup php /home/container/painel/artisan queue:work --queue=high,standard,low --sleep=3 --tries=3 &
 echo "🟢 Inicializado com sucesso"
