40557b5b39796bb86a5924230ea3457ee5bc42ad
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-02-03 01:52:52 -0400

    correção do cron

--- Paneldactyl/cron.sh
+++ Paneldactyl/cron.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 while :
 do
+  if [ "${PANEL}" = "Jexactyl" ] || [ "${PANEL}" = "Jexactyl Brasil" ]; then
+    HORA_CERTA='0000'
+    HORA=$(date +%H%M) # captura a hora
+    [ "$HORA" == "$HORA_CERTA" ] && php /var/www/jexactyl/artisan p:schedule:renewal >> /dev/null 2>&1 && break # executa o script desejado
+  fi
+  
   php /home/container/painel/artisan schedule:run >> /dev/null 2>&1 && /bin/bash /crontab_test.sh
   sleep 60
 done
-if [ "${PANEL}" = "Jexactyl" ] || [ "${PANEL}" = "Jexactyl Brasil" ]; then
-    HORA_CERTA='0000'
-
-    while :
-    do
-    HORA=$(date +%H%M) # captura a hora
-    [ "$HORA" == "$HORA_CERTA" ] && php /var/www/jexactyl/artisan p:schedule:renewal >> /dev/null 2>&1 && break # executa o script desejado
-    sleep 60 # espera um minuto pra tentar verificar novamente
-    done
-fi
