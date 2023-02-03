4780d3b89bc9cf424a3db031466b28d647be850c
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-02-02 23:19:31 -0400

    voltar

--- Frp/install.sh
+++ Frp/install.sh
@@ -3,7 +3,7 @@ if [[ -f "./Frps/frps" ]]; then
     bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Frp/start.sh)
 else
     mkdir -p /mnt/server
-    cd /mnt/server || exit
+    cd /mnt/server
 
     GITHUB_PACKAGE=fatedier/frp
     LATEST_JSON=$(curl --silent "https://api.github.com/repos/$GITHUB_PACKAGE/releases/latest" | jq -c '.[]' | head -1)
@@ -109,7 +109,7 @@ EOF
     if [ "${INSTALL_EX}" == "1" ]; then
         mkdir Exemplo_Frpc_Windows64
         cp -f ./Frpc/frpc.ini ./Exemplo_Frpc_Windows64/frpc.ini
-        cd Exemplo_Frpc_Windows64 || exit
+        cd Exemplo_Frpc_Windows64
             if [ -z "$VERSION" ] || [ "$VERSION" == "latest" ]; then
                 echo -e "defaulting to latest release"
                 DOWNLOAD_LINK_EX=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i windows | grep -i amd64)
