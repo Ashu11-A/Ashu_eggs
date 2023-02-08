20c7b74ebbccdb765bbbaba968854d624b9f1552
Matheus Nilton <matheusn.biolowons@gmail.com>
2023-02-08 17:29:38 -0400

    V1.7

--- Paneldactyl/version.sh
+++ Paneldactyl/version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 version_egg="1.0"
-version_script="1.6"
+version_script="1.7"
 echo "⚙️  Versão do Script: ${version_script}"
 if [[ -f "./logs/egg_version" ]]; then
     versions=" $(cat ./logs/egg_version) "
