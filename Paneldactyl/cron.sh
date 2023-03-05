8b28d535cbfce2d7acb1fa1483c1acd8c9cc2b38
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2023-03-04 20:22:20 -0400

    Correções

--- Paneldactyl/cron.sh
+++ Paneldactyl/cron.sh
@@ -4,8 +4,15 @@ do
   if [ "${PANEL}" = "Jexactyl" ] || [ "${PANEL}" = "Jexactyl Brasil" ]; then
     HORA_CERTA='0000'
     HORA=$(date +%H%M) # captura a hora
-    [ "$HORA" == "$HORA_CERTA" ] && php /var/www/jexactyl/artisan p:schedule:renewal >> /dev/null 2>&1 # executa o script desejado
+    [ "$HORA" == "$HORA_CERTA" ] && php /home/container/painel/artisan p:schedule:renewal >> /dev/null 2>&1
   fi
+
+  if [ "${PANEL}" = "Jexactyl" ] || [ "${PANEL}" = "Jexactyl Brasil" ] || [ "${PANEL}" = "Pterodactyl" ]; then
+    HORA_CERTA='0000'
+    HORA=$(date +%H%M) # captura a hora
+    [ "$HORA" == "$HORA_CERTA" ] && find ./backups/ -mindepth 1 -not -name "executado" -mtime +7 -delete # executa o script desejado
+  fi
+  
   
   php /home/container/painel/artisan schedule:run >> /dev/null 2>&1 && /bin/bash /crontab_test.sh
   sleep 60
