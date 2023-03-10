df65827b35f64a46887ff81c37d5c307bf561b5a
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-03-10 18:16:24 -0400

    V2.0
    Add Exemplo Linux64
    mudar ip auto:
    apenas-um.exemplo.net < ${SERVER_IP}

--- Frp/install.sh
+++ Frp/install.sh
@@ -16,19 +16,19 @@ else
             DOWNLOAD_LINK=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i linux | grep -i arm64)
         else
             VERSION_CHECK=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .tag_name')
-        if [ "$VERSION" == "$VERSION_CHECK" ]; then
-            DOWNLOAD_LINK=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .assets[].browser_download_url' | grep -i frp | grep -i linux | grep -i arm64)
-        else
-            echo -e "defaulting to latest release"
-            DOWNLOAD_LINK=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i linux | grep -i arm64)
-        fi
-        fi
-        else
-            if [ -z "$VERSION" ] || [ "$VERSION" == "latest" ]; then
-                echo -e "defaulting to latest release"
-                DOWNLOAD_LINK=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i linux | grep -i amd64)
+            if [ "$VERSION" == "$VERSION_CHECK" ]; then
+                DOWNLOAD_LINK=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .assets[].browser_download_url' | grep -i frp | grep -i linux | grep -i arm64)
             else
-                VERSION_CHECK=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .tag_name')
+                echo -e "defaulting to latest release"
+                DOWNLOAD_LINK=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i linux | grep -i arm64)
+            fi
+        fi
+    else
+        if [ -z "$VERSION" ] || [ "$VERSION" == "latest" ]; then
+            echo -e "defaulting to latest release"
+            DOWNLOAD_LINK=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i linux | grep -i amd64)
+        else
+            VERSION_CHECK=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .tag_name')
             if [ "$VERSION" == "$VERSION_CHECK" ]; then
                 DOWNLOAD_LINK=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .assets[].browser_download_url' | grep -i frp | grep -i linux | grep -i amd64)
             else
@@ -46,7 +46,7 @@ else
 
     mkdir Logs
 
-    cat <<EOF > ./Logs/log_install.txt
+    cat <<EOF >./Logs/log_install.txt
 Versão: ${VERSION}
 Link: ${DOWNLOAD_LINK}
 Arquivo: ${DOWNLOAD_LINK##*/}
@@ -60,10 +60,10 @@ EOF
     rm -rf frp*linux*
     rm -rf ${DOWNLOAD_LINK##*/}
 
-    cat <<EOF > frpc.ini
+    cat <<EOF >frpc.ini
 [common]
 #Aqui conecta no servidor externo (pode ser ip tambem)
-server_addr = apenas-um.exemplo.net
+server_addr = ${SERVER_IP}
 server_port = $bind_port
 bind_udp_port = $bind_udp_port
 #Aqui ira criar um http localmente para voce acessar
@@ -84,7 +84,7 @@ remote_port = 25310
 use_compression = true
 EOF
 
-    cat <<EOF > frps.ini
+    cat <<EOF >frps.ini
 [common]
 #As portas que serão usadas para permitir que seu frpc externo se conecte no seu servidor
 bind_port =
@@ -108,42 +108,65 @@ EOF
 
     if [ "${INSTALL_EX}" == "1" ]; then
         mkdir Exemplo_Frpc_Windows64
+        mkdir Exemplo_Frpc_Linux64
         cp -f ./Frpc/frpc.ini ./Exemplo_Frpc_Windows64/frpc.ini
-        cd Exemplo_Frpc_Windows64
-            if [ -z "$VERSION" ] || [ "$VERSION" == "latest" ]; then
-                echo -e "defaulting to latest release"
-                DOWNLOAD_LINK_EX=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i windows | grep -i amd64)
-            else
-                VERSION_CHECK=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .tag_name')
+        cp -f ./Frpc/frpc.ini ./Exemplo_Frpc_Linux64/frpc.ini
+        if [ -z "$VERSION" ] || [ "$VERSION" == "latest" ]; then
+            echo -e "defaulting to latest release"
+            DOWNLOAD_LINK_W=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i windows | grep -i amd64)
+            DOWNLOAD_LINK_L=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i linux | grep -i amd64)
+        else
+            VERSION_CHECK=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .tag_name')
             if [ "$VERSION" == "$VERSION_CHECK" ]; then
-                DOWNLOAD_LINK_EX=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .assets[].browser_download_url' | grep -i frp | grep -i windows | grep -i amd64)
+                DOWNLOAD_LINK_W=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .assets[].browser_download_url' | grep -i frp | grep -i windows | grep -i amd64)
+                DOWNLOAD_LINK_L=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .assets[].browser_download_url' | grep -i frp | grep -i linux | grep -i amd64)
             else
                 echo -e "defaulting to latest release"
-                DOWNLOAD_LINK_EX=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i windows | grep -i amd64)
+                DOWNLOAD_LINK_W=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i windows | grep -i amd64)
+                DOWNLOAD_LINK_L=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i frp | grep -i linux | grep -i amd64)
             fi
         fi
 
-        cat <<EOF > ../Logs/log_install_win.txt
+        cat <<EOF >../Logs/log_install_win.txt
 Versão: ${VERSION}
-Link: ${DOWNLOAD_LINK_EX}
-Arquivo: ${DOWNLOAD_LINK_EX##*/}
+Link Windows: ${DOWNLOAD_LINK_W}
+Arquivo Windows: ${DOWNLOAD_LINK_W##*/}
+Link Linux: ${DOWNLOAD_LINK_L}
+Arquivo Linux: ${DOWNLOAD_LINK_L##*/}
 EOF
-
-        echo -e "running 'curl -sSL ${DOWNLOAD_LINK_EX} -o ${DOWNLOAD_LINK_EX##*/}'"
-        curl -sSL ${DOWNLOAD_LINK_EX} -o ${DOWNLOAD_LINK_EX##*/}
-        echo -e "Unpacking server files"
-        unzip ${DOWNLOAD_LINK_EX##*/}
-        cp -R frp*/* ./
-        rm -rf frp*windows*
-        rm -rf ${DOWNLOAD_LINK_EX##*/}
-        rm frps*
-        rm LICENSE
-        cat <<EOF > start.bat
+        (
+            cd Exemplo_Frpc_Windows64 || exit
+            echo -e "running 'curl -sSL ${DOWNLOAD_LINK_W} -o ${DOWNLOAD_LINK_W##*/}'"
+            curl -sSL ${DOWNLOAD_LINK_W} -o ${DOWNLOAD_LINK_W##*/}
+            echo -e "Unpacking server files"
+            unzip ${DOWNLOAD_LINK_W##*/}
+            cp -R frp*/* ./
+            rm -rf frp*windows*
+            rm -rf ${DOWNLOAD_LINK_W##*/}
+            rm frps*
+            rm LICENSE
+            cat <<EOF >start.bat
 frpc.exe -c frpc.ini
 EOF
-        else
-            echo "Pulando Instalação do Exemplo Windows64"
-        fi
+        )
+        (
+            cd Exemplo_Frpc_Linux64 || exit
+            echo -e "running 'curl -sSL ${DOWNLOAD_LINK_L} -o ${DOWNLOAD_LINK_L##*/}'"
+            curl -sSL ${DOWNLOAD_LINK_L} -o ${DOWNLOAD_LINK_L##*/}
+            echo -e "Unpacking server files"
+            unzip ${DOWNLOAD_LINK_L##*/}
+            cp -R frp*/* ./
+            rm -rf frp*linux*
+            rm -rf ${DOWNLOAD_LINK_L##*/}
+            rm frps*
+            rm LICENSE
+            cat <<EOF >start.bat
+frpc.exe -c frpc.ini
+EOF
+        )
+    else
+        echo "Pulando Instalação do Exemplo Windows64 e Linux64"
+    fi
     echo -e "Instalação Completa"
     exit 0
 fi
