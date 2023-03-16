0313ed991a71d523b2026989d6fe30c56b3e9b40
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-03-16 17:41:53 -0400

    Update start.sh

--- Wings/start.sh
+++ Wings/start.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-if [[ -f "./wings" ]]; then
+#!/bin/ash
+# shellcheck shell=dash
+if [ -f "./wings" ]; then
     ./wings --config ./config.yml
 else
     bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Wings/install.sh) | tee logs/terminal.log
