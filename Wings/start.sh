576a3176f117840b359b26d3f60b05ba15a11dce
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-03-16 18:03:25 -0400

    add debug

--- Wings/start.sh
+++ Wings/start.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 # shellcheck shell=dash
 if [ -f "./wings" ]; then
-    ./wings --config ./config.yml
+    ./wings --debug --config ./config.yml
 else
     bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Wings/install.sh) | tee logs/terminal.log
 fi
