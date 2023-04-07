685ead54419715db60efea7835175e1ebecebf06
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-04-07 17:20:51 -0400

    Fix

--- Tdarr/launch.sh
+++ Tdarr/launch.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-rm -rf /home/container/tmp/*
-figlet -c -f ./Fonts/3d.flf "Tdarr" | lolcat
+# Define o nome do arquivo temporário
+temp_file=$(mktemp)
+
+# Executa o comando figlet e salva a saída no arquivo temporário
+figlet -c -f ./Fonts/3d.flf "Tdarr" > "$temp_file"
+
+# Executa o comando lolcat usando o arquivo temporário como entrada
+lolcat "$temp_file"
+
+# Remove o arquivo temporário
+rm "$temp_file"
+
+echo "🟢  Iniciando Tdarr_Updater..."
+nohup ./Tdarr_Updater >/dev/null 2>&1 &
 
 echo "🟢  Iniciando Tdarr_Server..."
 nohup ./Tdarr_Server/Tdarr_Server >/dev/null 2>&1 &
