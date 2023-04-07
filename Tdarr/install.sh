e2f0ef9a0f7a5dd40a26b462143a740c2dc62ce7
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-04-07 17:39:11 -0400

    Update install.sh

--- Tdarr/install.sh
+++ Tdarr/install.sh
@@ -2,8 +2,9 @@
 if [[ -f "./Tdarr_Updater" ]]; then
     bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Tdarr/launch.sh)
 else
-    mkdir -p /home/container
-    cd /home/container || exit
+    mkdir -p /mnt/server/
+    cd /mnt/server/ || exit
+    mkdir logs
     ARCH=$([ "$(uname -m)" == "x86_64" ] && echo "x64" || echo "arm64")
     echo "**** install tdarr package ****" && \
     DOWNLOAD_LINK=$(echo https://f000.backblazeb2.com/file/tdarrs/versions/2.00.15/linux_$ARCH/Tdarr_Updater.zip)
