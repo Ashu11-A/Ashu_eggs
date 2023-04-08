fd26000e935ba1efd1ef36eefdb9b89bb8b86fc9
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2023-04-08 17:43:50 -0400

    Update launch.sh

--- FFmpeg/launch.sh
+++ FFmpeg/launch.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 echo "🟢  Iniciando servidor..."
-npm run serve --port ${SERVER_PORT}
+nohup npm run serve 2>&1 &
+if [ "${SERVER_IP}" = "0.0.0.0" ]; then
+    MGM="na porta ${SERVER_PORT}"
+else
+    MGM="em ${SERVER_IP}:${SERVER_PORT}"
+fi
+echo "🟢  Interface auxiliar inicializada com sucesso ${MGM}..."
+
+while read -r line; do
+    if [[ "$line" == "ffmpeg" ]]; then
+
+        Comando1="${composer_start}"
+        echo "Instalando pacotes do Composer: ${bold}${lightblue}${Comando1}"
+        eval "cd /home/container/painel && $Comando1 && cd .."
+        printf "\n \n✅  Comando Executado\n \n"
+
+    elif [ "$line" != "${composer}" ] || [ "$line" != "${setup}" ] || [ "$line" != "${database}" ] || [ "$line" != "${migrate}" ] || [ "$line" != "${user_make}" ] || [ "$line" != "${yarn}" ]; then
+        echo "Comando Invalido, oque vocẽ está tentando fazer? tente ${bold}${lightblue}help"
+    else
+        echo "Script Falhou."
+    fi
+done
