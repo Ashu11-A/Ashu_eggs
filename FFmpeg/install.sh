4133fdc30394d76f8a190cfa9c7a3dbf30c443e5
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2023-04-08 14:45:27 -0400

    yeyyeeyye

--- FFmpeg/install.sh
+++ FFmpeg/install.sh
@@ -3,6 +3,9 @@ if [[ -d "./public" ]]; then
     bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/FFmpeg/version.sh)
     bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/FFmpeg/launch.sh)
 else
-    git clone https://github.com/alfg/ffmpeg-commander .
+    git clone https://github.com/alfg/ffmpeg-commander
+    mv ffmpeg-commander/* ./
+    rm -rf ffmpeg-commander
+    fakeroot chmod 775 *
     npm install
 fi
