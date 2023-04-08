f6a4198e293ff90daed34878e331c8a3db2746b8
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-04-08 18:03:32 -0400

    Update install.sh

--- FFmpeg/install.sh
+++ FFmpeg/install.sh
@@ -3,12 +3,15 @@ if [[ -d "./ffmpeg-commander/public" ]]; then
     bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/FFmpeg/version.sh)
     bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/FFmpeg/launch.sh)
 else
-    git clone https://github.com/Ashu11-A/ffmpeg-commander-Egg
-    mv ffmpeg-commander-Egg ffmpeg-commander
+    if [ ! -d "ffmpeg-commander" ]; then
+        git clone https://github.com/Ashu11-A/ffmpeg-commander-Egg
+        mv ffmpeg-commander-Egg ffmpeg-commander
+    fi
     fakeroot chmod 775 ./*
     (
         cd ffmpeg-commander || exit
         npm install
         npm audit fix
     )
+
 fi
