752fc271dc626ca6d0530d773733ba1ede067711
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-04-08 17:50:46 -0400

    Update launch.sh

--- FFmpeg/launch.sh
+++ FFmpeg/launch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+bold=$(echo -en "\e[1m")
+lightblue=$(echo -en "\e[94m")
+
 echo "🟢  Iniciando servidor..."
 nohup npm run serve 2>&1 &
+
 if [ "${SERVER_IP}" = "0.0.0.0" ]; then
     MGM="na porta ${SERVER_PORT}"
 else
@@ -9,15 +13,12 @@ fi
 echo "🟢  Interface auxiliar inicializada com sucesso ${MGM}..."
 
 while read -r line; do
-    if [[ "$line" == "ffmpeg" ]]; then
-
-        Comando1="${composer_start}"
-        echo "Instalando pacotes do Composer: ${bold}${lightblue}${Comando1}"
-        eval "cd /home/container/painel && $Comando1 && cd .."
+    if [[ "$line" == *"ffmpeg"* ]]; then
+        echo "Executando: ${bold}${lightblue}${line}"
+        eval "$line"
         printf "\n \n✅  Comando Executado\n \n"
-
-    elif [ "$line" != "${composer}" ] || [ "$line" != "${setup}" ] || [ "$line" != "${database}" ] || [ "$line" != "${migrate}" ] || [ "$line" != "${user_make}" ] || [ "$line" != "${yarn}" ]; then
-        echo "Comando Invalido, oque vocẽ está tentando fazer? tente ${bold}${lightblue}help"
+    elif [[ "$line" != *"ffmpeg"* ]]; then
+        echo "Comando Inválido. O que você está tentando fazer? Tente algo com ${bold}${lightblue}ffmpeg."
     else
         echo "Script Falhou."
     fi
