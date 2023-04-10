6549f0c7c2ba3131792b463f7f129223892e51d6
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-04-10 17:45:16 -0400

    Algumas correções e adições do launch

--- Paper Optimize/install.sh
+++ Paper Optimize/install.sh
@@ -4,7 +4,7 @@
 #
 # Server Files: /mnt/server
 if [ -f "${SERVER_JARFILE}" ]; then
-    java -Xmx"${SERVER_MEMORY}"M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -jar "${SERVER_JARFILE}"
+    bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Paper%20Optimize/launch.sh)
 else
     PROJECT=paper
     if [ -n "${DL_PATH}" ]; then
--- Paper Optimize/start.sh
+++ Paper Optimize/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Terraria/install.sh) | tee logs/terminal.log
+bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Paper%20Optimize/install.sh) | tee logs/terminal.log
