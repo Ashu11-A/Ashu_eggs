c6051f99d069a030c68c06715126bc1ae42ea0c1
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-04-10 18:08:39 -0400

    esqueci -_-

--- Paper Optimize/launch.sh
+++ Paper Optimize/launch.sh
@@ -31,6 +31,6 @@ else
     elif [ "${OPTIMIZE}" = "(5) 11+Gb RAM" ]; then
     START="java -Xms11G -Xmx11G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar ${SERVER_JARFILE}"
     echo "Executando otimização: ${OPTIMIZE} \n Com os argumentos: $START"
-
     fi
 fi
+$START
