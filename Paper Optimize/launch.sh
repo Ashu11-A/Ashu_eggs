44030bf14e341fd3e5a7b3bc5cfd310606aa871e
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-04-10 19:40:17 -0400

    Update launch.sh

--- Paper Optimize/launch.sh
+++ Paper Optimize/launch.sh
@@ -29,5 +29,5 @@ else
     START="java -Xms11G -Xmx11G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar ${SERVER_JARFILE}"
     fi
 fi
-printf "Executando otimização: ${bold}${lightblue}${OPTIMIZE} ${normal}\nCom os argumentos: ${bold}${lightblue}$START"
+printf "Executando otimização: ${bold}${lightblue}${OPTIMIZE} ${normal}\nCom os argumentos: ${bold}${lightblue}$START ${normal}"
 $START
