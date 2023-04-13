de60ec6734589b3c66e52e78e2a49b1c05ca7214
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-04-13 12:42:09 -0400

    Update install.sh

--- MTA/install.sh
+++ MTA/install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# troque
-# troque
 AMD64="https://linux.mtasa.com/dl/multitheftauto_linux_x64.tar.gz"
 ARM64="https://nightly.mtasa.com/multitheftauto_linux_arm64-1.5.9.tar.gz"
 ARCH=$([ "$(uname -m)" == "x86_64" ] && echo "$AMD64" || echo "$ARM64")
@@ -26,13 +24,21 @@ else
     if [ -d x64 ]; then
         if [ ! -f x64/libmysqlclient.so.16 ]; then
             curl -L http://nightly.mtasa.com/files/libmysqlclient.so.16 -o x64/libmysqlclient.so.16
+            chmod +x x64/libmysqlclient.so.16
         fi
+        mkdir x64/modules
+        curl -L https://nightly.mtasa.com/files/modules/64/mta_mysql.so -o x64/modules
+        curl -L https://nightly.mtasa.com/files/modules/64/ml_sockets.so -o x64/modules
     fi
 
     if [ -d arm64 ]; then
         if [ ! -f arm64/libmysqlclient.so.16 ]; then
             curl -L http://nightly.mtasa.com/files/libmysqlclient.so.16 -o arm64/libmysqlclient.so.16
+            chmod +x arm64/libmysqlclient.so.16
         fi
+        mkdir arm64/modules
+        curl -L https://nightly.mtasa.com/files/modules/64/mta_mysql.so -o arm64/modules
+        curl -L https://nightly.mtasa.com/files/modules/64/ml_sockets.so -o arm64/modules
     fi
 
     mkdir -p mods/deathmatch/resources
