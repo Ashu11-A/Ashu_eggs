e02038d3eabcf3f5cf221ba70949596053643d90
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-04-13 11:23:23 -0400

    ✅ V1.3

--- MTA/start.sh
+++ MTA/start.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 ARCH=$([ "$(uname -m)" == "x86_64" ] && echo "amd64" || echo "arm64")
 
-echo "⚙️  Versão do Script: 1.8"
+echo "⚙️  Script Version: 1.9"
 
 if [ "${ARCH}" == "amd64" ];
 then
-    echo "🔎 Arquitetura Identificada: 64x"
+    echo "🔎  Identified Architecture: 64x"
     if [[ -f "./mta-server64" ]]; then
-        echo "✅ Iniciando MTA"
+        echo "✅  Starting MTA"
         ./mta-server64 --maxplayers ${MAX_PLAYERS} --port ${SERVER_PORT} --httpport ${SERVER_WEBPORT} -n
     else
-        echo "MTA Não Instalado, isso é realmente muito estranho, essa é uma segunda verificação."
+        echo "pt-BR: MTA Não Instalado, isso é realmente muito estranho, essa é uma segunda verificação."
+        echo "en: MTA Not Installed, this is really very strange, this is a second check."
     fi
 else
-    echo "🔎 Arquitetura Identificada: ARM64"
-    echo "⚠️ Atenção: Este Egg ainda não funciona no ARM64"
-    if [[ -f "./mta-server64" ]]; then
-        echo "✅ Iniciando MTA"
-        ./mta-server64 --maxplayers ${MAX_PLAYERS} --port ${SERVER_PORT} --httpport ${SERVER_WEBPORT} -n
+    echo "🔎  Identified Architecture: ARM64"
+    if [[ -f "./mta-server-arm64" ]]; then
+        echo "✅  Starting MTA"
+        ./mta-server-arm64 --maxplayers ${MAX_PLAYERS} --port ${SERVER_PORT} --httpport ${SERVER_WEBPORT} -n
     else
-        echo "MTA Não Instalado, isso é realmente muito estranho, essa é uma segunda verificação."
+        echo "pt-BR: MTA Não Instalado, isso é realmente muito estranho, essa é uma segunda verificação."
+        echo "en: MTA Not Installed, this is really very strange, this is a second check."
     fi
 fi
