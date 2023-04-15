756a7b0b95beb098e05ea57d778eeb27283b252e
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-04-15 18:35:31 -0400

    yeye

--- Tachidesk/install.sh
+++ Tachidesk/install.sh
@@ -2,19 +2,30 @@
 if [[ ! -f "./Tachidesk-Server.jar" ]]; then
 
     GITHUB_PACKAGE=Suwayomi/Tachidesk-Server
-    LATEST_JSON=$(curl --silent "https://api.github.com/repos/$GITHUB_PACKAGE/releases" | jq -c '.[]' | head -1)
-    RELEASES=$(curl --silent "https://api.github.com/repos/$GITHUB_PACKAGE/releases" | jq '.[]')
+    LATEST_JSON=$(curl --silent "https://api.github.com/repos/$GITHUB_PACKAGE/releases")
+    if echo "$LATEST_JSON" | jq empty >/dev/null; then
+        echo "Erro: resposta inválida da API do GitHub."
+        exit 1
+    fi
+
+    LATEST_JSON=$(echo "$LATEST_JSON" | jq -c '.[]' | head -1)
+    RELEASES=$(echo "$LATEST_JSON" | jq '.assets')
     if [ -z "$VERSION" ] || [ "$VERSION" == "latest" ]; then
         echo -e "defaulting to latest release"
-        DOWNLOAD_LINK=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i Tachidesk | grep -i jar)
+        DOWNLOAD_LINK=$(echo "$RELEASES" | jq -r '.[] | select(.name | test("Tachidesk.*jar$"; "i")).browser_download_url')
     else
-        VERSION_CHECK=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .tag_name')
+        VERSION_CHECK=$(echo "$LATEST_JSON" | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .tag_name')
         if [ "$VERSION" == "$VERSION_CHECK" ]; then
-            DOWNLOAD_LINK=$(echo $RELEASES | jq -r --arg VERSION "$VERSION" '. | select(.tag_name==$VERSION) | .assets[].browser_download_url' | grep -i Tachidesk | grep -i jar)
+            DOWNLOAD_LINK=$(echo "$RELEASES" | jq -r --arg VERSION "$VERSION" '.[] | select(.name | test("Tachidesk.*jar$"; "i")).browser_download_url')
         else
             echo -e "defaulting to latest release"
-            DOWNLOAD_LINK=$(echo $LATEST_JSON | jq .assets | jq -r .[].browser_download_url | grep -i Tachidesk | grep -i jar)
+            DOWNLOAD_LINK=$(echo "$RELEASES" | jq -r '.[] | select(.name | test("Tachidesk.*jar$"; "i")).browser_download_url')
         fi
+    fi
+
+    if [ -z "$DOWNLOAD_LINK" ]; then
+        echo "Não foi possível encontrar o link de download."
+        exit 1
     fi
 
     if [ ! -d "./Logs" ]; then
@@ -30,7 +41,7 @@ EOF
     fi
 
     echo -e "running 'curl -sSL ${DOWNLOAD_LINK} -o ${DOWNLOAD_LINK##*/}'"
-    curl -sSL ${DOWNLOAD_LINK} -o Tachidesk-Server.jar
+    curl -sSL "$DOWNLOAD_LINK" -o "${DOWNLOAD_LINK##*/}"
 fi
 
 if [ ! -d "./Config" ]; then
