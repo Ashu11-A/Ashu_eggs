91527b2db9dbb554c9921337000136a4ba488dc6
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-04-25 19:09:46 -0400

    Culpa do mono, mas tá ai

--- Terraria/launch.sh
+++ Terraria/launch.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # shellcheck shell=dash
-if [ "${FAKETIME}" = "1" ]; then
-    faketime "$ANO_DIA" /bin/date
-    echo "O horário de agora é:"
-    date
-fi
 
 if [ "${TERRARIA_VERSION}" = "1.4.4" ] || [ "${TERRARIA_VERSION}" = "144" ]; then
-    mono --server --gc=sgen -O=all ./TerrariaServer.exe -ip 0.0.0.0 -port {{SERVER_PORT}}  -maxplayers {{MAX_PLAYERS}} -difficulty {{DIFFICULTY}} -pass {{PASSWORD}} -motd {{MOTD}} -world /home/container/.local/share/Terraria/Worlds/{{WORLD_NAME}}.wld -autocreate {{WORLD_SIZE}} -banlist /home/container/banlist.txt -worldname {{WORLD_NAME}} -secure {{SECURE}} -seed {{SEED}}
+    if [ "${FAKETIME}" = "1" ]; then
+        LD_PRELOAD=/usr/lib/faketime/libfaketime.so.1 FAKETIME="$ANO_DIA" mono --server --gc=sgen -O=all ./TerrariaServer.exe -ip 0.0.0.0 -port {{SERVER_PORT}} -maxplayers {{MAX_PLAYERS}} -difficulty {{DIFFICULTY}} -pass {{PASSWORD}} -motd {{MOTD}} -world /home/container/.local/share/Terraria/Worlds/{{WORLD_NAME}}.wld -autocreate {{WORLD_SIZE}} -banlist /home/container/banlist.txt -worldname {{WORLD_NAME}} -secure {{SECURE}} -seed {{SEED}}
+    else
+        mono --server --gc=sgen -O=all ./TerrariaServer.exe -ip 0.0.0.0 -port {{SERVER_PORT}} -maxplayers {{MAX_PLAYERS}} -difficulty {{DIFFICULTY}} -pass {{PASSWORD}} -motd {{MOTD}} -world /home/container/.local/share/Terraria/Worlds/{{WORLD_NAME}}.wld -autocreate {{WORLD_SIZE}} -banlist /home/container/banlist.txt -worldname {{WORLD_NAME}} -secure {{SECURE}} -seed {{SEED}}
+    fi
 else
-    mono --server --gc=sgen -O=all  ./TerrariaServer.exe -config serverconfig.txt
+    if [ "${FAKETIME}" = "1" ]; then
+        LD_PRELOAD=/usr/lib/faketime/libfaketime.so.1 FAKETIME="$ANO_DIA" mono --server --gc=sgen -O=all ./TerrariaServer.exe -config serverconfig.txt
+    else
+        mono --server --gc=sgen -O=all ./TerrariaServer.exe -config serverconfig.txt
+    fi
 fi
