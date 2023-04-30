b71ad2baa968e60ea7c87d154a539894eec007f1
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-04-30 11:44:57 -0400

    mude o local das logs, nada de arquivos na pasta home!

--- Bot4All/start.sh
+++ Bot4All/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Bot4All/install.sh) | tee logs/terminal.log
+bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Bot4All/install.sh) | tee ../terminal.log
