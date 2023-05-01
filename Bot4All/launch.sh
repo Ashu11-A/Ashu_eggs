76f196592f07839eac686e5e100068f80cfe4502
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-04-30 21:58:39 -0400

    fix

--- Bot4All/launch.sh
+++ Bot4All/launch.sh
@@ -39,7 +39,7 @@ echo "🟢  Estou rodando ${MGM}..."
     fi
 )
 
-echo "📃  Comandos Disponíveis: ${bold}${lightblue}help ${normal}, ${bold}${lightblue}version ${normal}, ${bold}${lightblue}npm ${normal}[your code] ou ${bold}${lightblue}node ${normal}[your code]..."
+printf "\n \n%s📃 Comandos Disponíveis:%s %shelp%s, %sversion%s, %snpm%s[your code] ou %snode%s[your code]...\n \n" "$bold" "$normal" "$bold" "$normal" "$bold" "$normal" "$bold" "$normal" "$bold" "$normal"
 
 start="$(cat logs/start-conf)"
 (
