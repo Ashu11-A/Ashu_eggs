48f59fde35625e97aae4db4e99c60a6260ce1142
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-05-01 20:00:11 -0400

    Update start.sh

--- Bot4All/start.sh
+++ Bot4All/start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Bot4All/install.sh) | tee logs/terminal.log
+
+: <<'LIMBO'
 lolcat=/usr/games/lolcat
-bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Bot4All/install.sh) | tee logs/terminal.log | $lolcat
+
+nohup bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Bot4All/install.sh) >logs/terminal.log 2>&1 &
+
+tail -f logs/terminal.log | $lolcatBO
+LIMBO
