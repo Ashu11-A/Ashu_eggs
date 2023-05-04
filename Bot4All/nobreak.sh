d9fb8ef85294887f09cbd074e424f8f1a2963f24
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-05-04 16:24:30 -0400

    Update nobreak.sh

--- Bot4All/nobreak.sh
+++ Bot4All/nobreak.sh
@@ -2,6 +2,7 @@
 while true; do
     start="$(cat logs/start-conf)"
     (
+        npm config set prefix /home/container/.npm
         cd "./[seu_bot]" || exit
         node ${start} >log_egg.txt 2>&1
 
