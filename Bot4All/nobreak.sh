ea4b7148143c9ad9d944c1cb53f620d99418d480
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-05-04 17:59:29 -0400

    Quebre só 10 vezes

--- Bot4All/nobreak.sh
+++ Bot4All/nobreak.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-while true; do
+count=0
+while [ $count -lt 10 ]; do
     start="$(cat logs/start-conf)"
     (
-        
         node ${start} >log_egg.txt 2>&1
-
         npm start >log_egg.txt 2>&1
-
         sleep 1
     )
+    count=$((count+1))
 done
