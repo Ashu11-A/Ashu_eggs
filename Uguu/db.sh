9f4a7f5e63b6727b890d7c9e0e03316c44628366
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-05-07 16:53:43 -0400

    oooo puta

--- Uguu/db.sh
+++ Uguu/db.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 ## Instalando Postgres
 if [ ! -f "./logs/database_instalado" ]; then
-    if [ "$PWD" = "/" ]; then
-        # Comandos a serem executados se estiver na pasta /mnt/server/ yeye
-        # Server Files: /mnt/server yeye
-        adduser -D -h /home/container container
-        chown -R container: /mnt/server/
-        su container -c 'initdb -D /mnt/server/DB/ -A md5 -U "$PGUSER" --pwfile=<(echo "$PGPASSWORD")'
-        mkdir -p /mnt/server/DB/run/
-        ## Add default "allow from all" auth rule to pg_hba
-        if ! grep -q "# Custom rules" "/mnt/server/DB/pg_hba.conf"; then
-            echo -e "# Custom rules\nhost all all 0.0.0.0/0 md5" >>"/mnt/server/DB/pg_hba.conf"
-        fi
-        echo -e "Done"
-        touch ./logs/database_instalado
-    else
-        # Comandos a serem executados se NÃO estiver na pasta /mnt/server/ yeye
-        echo "📢  ATENÇÃO: Não tenho acesso root, então não posso instalar o Database, reinstale o Egg!"
+    # Comandos a serem executados se estiver na pasta /mnt/server/
+    # Server Files: /mnt/server
+    adduser -D -h /home/container container
+    chown -R container: /mnt/server/
+    su container -c 'initdb -D /mnt/server/DB/ -A md5 -U "$PGUSER" --pwfile=<(echo "$PGPASSWORD")'
+    mkdir -p /mnt/server/DB/run/
+    ## Add default "allow from all" auth rule to pg_hba
+    if ! grep -q "# Custom rules" "/mnt/server/DB/pg_hba.conf"; then
+        echo -e "# Custom rules\nhost all all 0.0.0.0/0 md5" >>"/mnt/server/DB/pg_hba.conf"
     fi
+    echo -e "Done"
+    if [ ! -d "/mnt/server/logs" ]; then
+        mkdir logs
+    fi
+    touch /mnt/server/logs/database_instalado
 fi
