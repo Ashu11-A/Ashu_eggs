c61cf7350ea43532b9cf7d49a9eb4b866b3d8b3b
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-05-07 16:47:38 -0400

    puta

--- Uguu/db.sh
+++ Uguu/db.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 ## Instalando Postgres
 if [ ! -f "./logs/database_instalado" ]; then
-    if [ "$PWD" = "/mnt/server/" ]; then
+    if [ "$PWD" = "/mnt/server" ]; then
         # Comandos a serem executados se estiver na pasta /mnt/server/
         # Server Files: /mnt/server
-        apt update
-        apt upgrade -y
-        apt install -y curl wget unzip git tar ca-certificates jq fuse
         adduser -D -h /home/container container
         chown -R container: /mnt/server/
         su container -c 'initdb -D /mnt/server/DB/ -A md5 -U "$PGUSER" --pwfile=<(echo "$PGPASSWORD")'
@@ -15,6 +12,7 @@ if [ ! -f "./logs/database_instalado" ]; then
         if ! grep -q "# Custom rules" "/mnt/server/DB/pg_hba.conf"; then
             echo -e "# Custom rules\nhost all all 0.0.0.0/0 md5" >>"/mnt/server/DB/pg_hba.conf"
         fi
+        echo -e "Done"
         touch ./logs/database_instalado
     else
         # Comandos a serem executados se NÃO estiver na pasta /mnt/server/
