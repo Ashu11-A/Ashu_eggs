85ae843e7ac7768ba28fea4f40624cb6eb5a0601
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2023-06-10 21:21:08 -0400

    Update start.sh

--- tModLoader/start.sh
+++ tModLoader/start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 echo "⚙️  Versão do Script: 1.3"
-
-if [[ -f "./tModLoader.dll" ]]; then
-    echo "✅ Iniciando tModLoader"
-    dotnet tModLoader.dll -server "$@" -config serverconfig.txt
-else
-    echo "tModLoader Não Instalado, isso é realmente muito estranho, essa é uma segunda verificação."
-fi
+echo "✅ Iniciando tModLoader"
+dotnet tModLoader.dll -server "$@" -config serverconfig.txt
