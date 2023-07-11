3bb349995c7a9c2bcc48fda2fa01715ef9dbeab3
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-07-11 17:27:37 -0400

    Stress EN

--- Connect/en/Stress/start.sh
+++ Connect/en/Stress/start.sh
@@ -1,19 +1,20 @@
 #!/bin/ash
+# shellcheck shell=dash
 
 MCS=$([ "${METRICS}" == "1" ] && echo "--metrics-brief" || echo "")
-CPU_ON=$([ "${CPU}" == "1" ] && echo "--cpu ${CPU_CORES}" || echo "") 
+CPU_ON=$([ "${CPU}" == "1" ] && echo "--cpu ${CPU_CORES}" || echo "")
 CPU_RAM=$([ "${CPU_MEMORY}" == "1" ] && echo "--vm ${CPU_CORES} --vm-bytes ${MEMORY}M" || echo "")
 TIME=$([ "${TIMEOUT}" == "0" ] && echo "" || echo "--timeout ${TIMEOUT}s")
 
-echo "⚙️  Versão do Script: 1.2"
+echo "⚙️ Script Version: 1.2"
 
-echo "✓ Atualizando o script..."
+echo "✓ Updating the script..."
 curl -o start.sh https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Connect/pt-BR/Stress/start.sh
 
 if [ "${CONSOLE}" == "1" ]; then
-    echo "Executando: stress-ng ${CONSOLE_OCC}"
+    echo "Executing: stress-ng ${CONSOLE_OCC}"
     stress-ng ${CONSOLE_OCC}
- else
-    echo "Executando: stress-ng ${CPU_ON} ${CPU_RAM} ${MCS} ${TIME}"
+else
+    echo "Executing: stress-ng ${CPU_ON} ${CPU_RAM} ${MCS} ${TIME}"
     stress-ng ${CPU_ON} ${CPU_RAM} ${MCS} ${TIME}
 fi
