48945614df01a8dae12d2228855f24debbb2063a
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-07-24 18:07:39 -0400

    fix

--- Connect/pt-BR/CtrlPanel/launch.sh
+++ Connect/pt-BR/CtrlPanel/launch.sh
@@ -20,7 +20,7 @@ normal=$(echo -en "\e[0m")
 rm -rf /home/container/tmp/*
 
 echo "🟢  Iniciando PHP-FPM..."
-nohup /usr/sbin/php-fpm81 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize >/dev/null 2>&1 &
+nohup /usr/sbin/php-fpm8.1 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize >/dev/null 2>&1 &
 
 echo "🟢  Iniciando Nginx..."
 nohup /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/ >/dev/null 2>&1 &
