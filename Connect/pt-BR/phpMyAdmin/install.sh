319c48ae8d2cc7a836a23cdba9617c26b35ee5b8
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-07-26 17:30:05 -0400

    phpMyAdmin (Oh ma ga)

---
+++ Connect/pt-BR/phpMyAdmin/install.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+if [ -d "/home/container/controlpanel" ]; then
+    printf "\n \n📄  Verificando Instalação...\n \n"
+    printf "+----------+---------------------------------+\n| Tarefa   | Status                          |\n+----------+---------------------------------+"
+    printf "\n| phpMyAdmin | 🟢  Instalado                    |"
+else
+    printf "\n \n📄  Verificando Instalação...\n \n"
+    printf "+----------+---------------------------------+\n| Tarefa   | Status                          |\n+----------+---------------------------------+"
+    printf "\n| phpMyAdmin | 🟡  Baixando phpMyAdmin               |\n"
+
+    wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-english.tar.gz
+    mkdir phpMyAdmin
+    echo -e "Unpacking server files"
+    tar xvzf phpMyAdmin-latest-english.tar.gz
+    mv phpMyAdmin-*-english/* phpMyAdmin
+    rm -rf phpMyAdmin-*-english
+    mv "${DOWNLOAD_LINK##*/}" controlpanel
+    fakeroot chmod -R 755 ./*
+fi
+
+if [ -d "temp" ]; then ## Evita conflitos no painel pelo comando seguinte do git
+    rm -rf temp
+fi
+
+git clone --quiet https://github.com/Ashu11-A/nginx ./temp ## Sim, ele sempre irá clonar o repo idenpendente de tudo
+if [ -f "/home/container/nginx/nginx.conf" ]; then
+    printf "\n| Nginx    | 🟢  Instalado                    |"
+else
+    printf "\n| Nginx    | 🟡  Baixando Nginx...            |"
+    cp -r ./temp/nginx ./
+    rm nginx/conf.d/default.conf
+    curl -sSL https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Connect/pt-BR/phpMyAdmin/default.conf -o ./nginx/conf.d/default.conf
+    sed -i \
+        -e "s/listen.*/listen ${SERVER_PORT};/g" \
+        nginx/conf.d/default.conf
+fi
+if [ -d "/home/container/php-fpm" ]; then
+    printf "\n| PHP-FPM  | 🟢  Instalado                    |\n+----------+---------------------------------+\n"
+else
+    printf "\n| PHP-FPM  | 🟡  Baixando PHP-FPM...          |\n+----------+---------------------------------+\n"
+    cp -r ./temp/php-fpm ./
+    echo "extension=\"smbclient.so\"" >php-fpm/conf.d/00_smbclient.ini
+    echo 'apc.enable_cli=1' >>php-fpm/conf.d/apcu.ini
+    sed -i \
+        -e 's/;opcache.enable.*=.*/opcache.enable=1/g' \
+        -e 's/;opcache.interned_strings_buffer.*=.*/opcache.interned_strings_buffer=16/g' \
+        -e 's/;opcache.max_accelerated_files.*=.*/opcache.max_accelerated_files=10000/g' \
+        -e 's/;opcache.memory_consumption.*=.*/opcache.memory_consumption=128/g' \
+        -e 's/;opcache.save_comments.*=.*/opcache.save_comments=1/g' \
+        -e 's/;opcache.revalidate_freq.*=.*/opcache.revalidate_freq=1/g' \
+        -e 's/;always_populate_raw_post_data.*=.*/always_populate_raw_post_data=-1/g' \
+        -e 's/memory_limit.*=.*128M/memory_limit=512M/g' \
+        -e 's/max_execution_time.*=.*30/max_execution_time=120/g' \
+        -e 's/upload_max_filesize.*=.*2M/upload_max_filesize=1024M/g' \
+        -e 's/post_max_size.*=.*8M/post_max_size=1024M/g' \
+        -e 's/output_buffering.*=.*/output_buffering=0/g' \
+        php-fpm/php.ini
+    sed -i \
+        '/opcache.enable=1/a opcache.enable_cli=1' \
+        php-fpm/php.ini
+    echo "env[PATH] = /usr/local/bin:/usr/bin:/bin" >>php-fpm/php-fpm.conf
+fi
+
+cp -r ./temp/logs ./
+
+if [[ -f "./logs/phpMyAdmin_instalado" ]]; then
+    echo "+----------+---------------------------------+"
+    printf "\n \n📑  Verificação do Painel Concluída...\n \n"
+else
+    printf "\n \n⚙️  Executando: Atribuição de permissões\n \n"
+    fakeroot chown -R nginx:nginx /home/container/controlpanel/*
+    printf "\n \n⚙️  Instalação do painel concluída\n \n"
+    touch ./logs/phpMyAdmin_instalado
+fi
+
+#Executando Limpeza
+if [ -d "tmp" ]; then
+    rm -rf tmp/*
+fi
+if [ -d "temp" ]; then
+    rm -rf temp
+fi
+if [ -d ".composer" ]; then
+    rm -rf .composer
+fi
+if [ -d ".yarn" ]; then
+    rm -rf .yarn
+fi
+if [ -d ".cache" ]; then
+    rm -rf .cache
+fi
+if [ -f ".yarnrc" ]; then
+    rm -rf .yarnrc
+fi
+
+if [[ -f "./logs/phpMyAdmin_instalado" ]]; then
+    bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Connect/pt-BR/phpMyAdmin/version.sh)
+    bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Connect/pt-BR/phpMyAdmin/launch.sh)
+else
+    echo "Algo muito errado aconteceu."
+fi
+
+: <<'LIMBO'
+LIMBO
---
+++ Connect/pt-BR/phpMyAdmin/launch.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+reinstall_a="reinstall all"
+reinstall_a_start="rm -rf controlpanel && rm -rf logs/panel* && rm -rf nginx && rm -rf php-fpm"
+
+reinstall_p="reinstall controlpanel"
+reinstall_p_start="rm -rf controlpanel && rm -rf logs/panel*"
+
+reinstall_n="reinstall nginx"
+reinstall_n_start="rm -rf nginx"
+
+reinstall_f="reinstall php-fpm"
+reinstall_f_start="rm -rf php-fpm"
+
+bold=$(echo -en "\e[1m")
+lightblue=$(echo -en "\e[94m")
+normal=$(echo -en "\e[0m")
+rm -rf /home/container/tmp/*
+
+echo "🟢  Iniciando PHP-FPM..."
+nohup /usr/sbin/php-fpm81 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize >/dev/null 2>&1 &
+
+echo "🟢  Iniciando Nginx..."
+nohup /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/ >/dev/null 2>&1 &
+if [ "${SERVER_IP}" = "0.0.0.0" ]; then
+    MGM="na porta ${SERVER_PORT}"
+else
+    MGM="em ${SERVER_IP}:${SERVER_PORT}"
+fi
+echo "🟢  Inicializado com sucesso ${MGM}..."
+
+echo "📃  Comandos Disponíveis:${bold}${lightblue}reinstall${normal}..."
+
+while read -r line; do
+    if [[ "$line" == "help" ]]; then
+        echo "Comandos Disponíveis:"
+        echo "
++-----------+---------------------------------------+
+| Comando   |  O que Faz                            |
++-----------+---------------------------------------+
+| reinstall |  Reinstala algo ou tudo               |
++-----------+---------------------------------------+
+        "
+
+    elif [[ "$line" == "${reinstall_a}" ]]; then
+
+        echo "📌  Reinstalando o controlpanel, nginx e php-fpm..."
+        printf "\n \n⚠️  Tem certeza que deseja Reinstalar? [y/N]\n \n"
+        read -r response
+        case "$response" in
+        [yY][eE][sS] | [yY])
+            ${reinstall_a_start}
+            printf "\n \n✅  Comando Executado\n \n"
+            exit
+            ;;
+        *)
+            printf "\n \n❌  Comando Não Executado\n \n"
+            ;;
+        esac
+
+    elif [[ "$line" == "${reinstall_p}" ]]; then
+
+        echo "📌  Reinstalando o Controlpanel..."
+        printf "\n \n⚠️  Tem certeza que deseja Reinstalar? [y/N]\n \n"
+        read -r response
+        case "$response" in
+        [yY][eE][sS] | [yY])
+            ${reinstall_p_start}
+            printf "\n \n✅  Comando Executado\n \n"
+            exit
+            ;;
+        *)
+            printf "\n \n❌  Comando Não Executado\n \n"
+            ;;
+        esac
+
+    elif [[ "$line" == "${reinstall_n}" ]]; then
+
+        echo "📌  Reinstalando o Nginx..."
+        printf "\n \n⚠️  Tem certeza que deseja Reinstalar? [y/N]\n \n"
+        read -r response
+        case "$response" in
+        [yY][eE][sS] | [yY])
+            ${reinstall_n_start}
+            printf "\n \n✅  Comando Executado\n \n"
+            exit
+            ;;
+        *)
+            printf "\n \n❌  Comando Não Executado\n \n"
+            ;;
+        esac
+
+    elif [[ "$line" == "${reinstall_f}" ]]; then
+
+        echo "📌  Reinstalando o PHP-FPM..."
+        printf "\n \n⚠️  Tem certeza que deseja Reinstalar? [y/N]\n \n"
+        read -r response
+        case "$response" in
+        [yY][eE][sS] | [yY])
+            ${reinstall_f_start}
+            printf "\n \n✅  Comando Executado\n \n"
+            exit
+            ;;
+        *)
+            printf "\n \n❌  Comando Não Executado\n \n"
+            ;;
+        esac
+
+    elif [[ "$line" != "reinstall"* ]]; then
+        echo "Comando Invalido, oque vocẽ está tentando fazer? tente ${bold}${lightblue}help"
+    else
+        echo "Script Falhou."
+    fi
+done
