622345e9ea9bcd01d16ffe150842ce04c0eec3ad
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-07-26 19:50:58 -0400

    ouw, this is a lolcat

--- Connect/pt-BR/phpMyAdmin/start.sh
+++ Connect/pt-BR/phpMyAdmin/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-lolcat=/usr/games/lolcat
-echo -e "\n \n$(figlet -c -f slant -t -k "phpMyAdmin")\n                                         by Ashu" | $lolcat
+echo -e "\n \n$(figlet -c -f slant -t -k "phpMyAdmin")\n                                         by Ashu" | lolcat
 
 bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Connect/pt-BR/phpMyAdmin/install.sh) | tee logs/terminal.log
