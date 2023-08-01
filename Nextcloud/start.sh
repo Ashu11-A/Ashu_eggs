ed281ad8eefb4b3bbc6e822fb4a49d5019516231
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-08-01 16:47:15 -0400

    Limpando...

--- Nextcloud/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/ash
-rm -rf /home/container/tmp/*
-echo "⚙️ Versão do Script: 1.8"
-echo "🛠 Iniciando PHP-FPM..."
-/usr/sbin/php-fpm81 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
-
-echo "🛠 Iniciando Nginx..."
-echo "✅ Inicializado com sucesso"
-/usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
