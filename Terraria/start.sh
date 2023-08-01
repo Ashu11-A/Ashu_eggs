21450dab0f21e12875a20e361cd506f810118dee
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-08-01 18:20:07 -0400

    Desculpe-me

--- Terraria/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Terraria/install.sh) | tee logs/terminal.log
--- tModLoader/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "⚙️  Versão do Script: 1.3"
-echo "✅ Iniciando tModLoader"
-dotnet tModLoader.dll -server "$@" -config serverconfig.txt
