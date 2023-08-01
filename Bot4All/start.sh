ed46187b916e97724cf21b8104cd2d56ea2c3bcf
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-08-01 16:46:41 -0400

    limpando...

--- Bot4All/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -f "./install.sh" ]; then
-    rm ./install.sh
-fi
-
-curl -sO https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Bot4All/install.sh
-chmod +x install.sh
-./install.sh
-
