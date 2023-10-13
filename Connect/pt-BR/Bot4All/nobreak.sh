5bf12ab5d574487bbf45e4352f70e2a52fe3d4b9
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-10-13 14:08:39 -0400

    🍷 Add BREAK_NUMBER

--- Connect/pt-BR/Bot4All/nobreak.sh
+++ Connect/pt-BR/Bot4All/nobreak.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 count=0
-while [ $count -lt 10 ]; do
+while [ $count -lt "$BREAK_NUMBER" ]; do
     start="$(cat logs/start-conf)"
     # Verifica se o conteúdo é um arquivo existente
     if [ -f "$start" ]; then
         # Verifica se o arquivo é um arquivo JavaScript (.js)
         if [[ "$start" == *.js ]]; then
-            node ${start} >logs/run.log 2>&1
+            node "$start" >logs/run.log 2>&1
             sleep 1
         fi
     elif [[ "$start" == "npm run"* ]] || [[ "$start" == "node"* ]]; then
