d775632e4425b88ea5471c64d947f373910cce8a
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-11-24 17:26:24 -0400

    Typebot: base code

---
+++ Connect/pt-BR/Typebot/launch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+rm -rf /home/container/tmp/*
+
+# echo "🟢  Iniciando PHP-FPM..."
+# nohup /usr/sbin/php-fpm8.1 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize >/dev/null 2>&1 &
+
+# echo "🟢  Iniciando Nginx..."
+# nohup /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/ >/dev/null 2>&1 &
+# if [ "${SERVER_IP}" = "0.0.0.0" ]; then
+#     MGM="na porta ${SERVER_PORT}"
+# else
+#     MGM="em ${SERVER_IP}:${SERVER_PORT}"
+# fi
+# echo "🟢  Inicializado com sucesso ${MGM}..."
+(
+    cd apps/builder || exit
+    pnpm start
+    pnpm start -p "${SERVER_PORT}"
+)
---
+++ Connect/pt-BR/Typebot/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+lolcat=/usr/games/lolcat
+echo -e "\n \n$(figlet -c -f slant -t -k "Typebot")\n                                         by Ashu" | $lolcat
+
+bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Connect/pt-BR/Typebot/install.sh) | tee logs/terminal.log
