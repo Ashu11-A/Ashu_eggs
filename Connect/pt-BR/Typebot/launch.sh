47e32a7195e10477e9b7c42c5a7d8b467961bf54
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-11-24 17:27:30 -0400

    Update launch.sh

--- Connect/pt-BR/Typebot/launch.sh
+++ Connect/pt-BR/Typebot/launch.sh
@@ -6,12 +6,12 @@ rm -rf /home/container/tmp/*
 
 # echo "🟢  Iniciando Nginx..."
 # nohup /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/ >/dev/null 2>&1 &
-# if [ "${SERVER_IP}" = "0.0.0.0" ]; then
-#     MGM="na porta ${SERVER_PORT}"
-# else
-#     MGM="em ${SERVER_IP}:${SERVER_PORT}"
-# fi
-# echo "🟢  Inicializado com sucesso ${MGM}..."
+if [ "${SERVER_IP}" = "0.0.0.0" ]; then
+    MGM="na porta ${SERVER_PORT}"
+else
+    MGM="em ${SERVER_IP}:${SERVER_PORT}"
+fi
+echo "🟢  Inicializado com sucesso ${MGM}..."
 (
     cd apps/builder || exit
     pnpm start
