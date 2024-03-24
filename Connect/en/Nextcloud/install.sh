f88828566ed92d82ab21def557c17ef2840c5a36
Ashu11-A <Matheusn.biolowons@gmail.com>
2024-03-24 19:08:04 -0400

    📥 Merge: Nextcloud (pt-BR => EN)

--- Connect/en/Nextcloud/install.sh
+++ Connect/en/Nextcloud/install.sh
@@ -55,27 +55,27 @@ rm -rf ${DOWNLOAD_LINK}
 
 chown -R nginx:nginx nextcloud && chmod -R 755 nextcloud
 echo "**** Cleaning up "
-rm -rf /tmp/
-echo "* Configuring PHP and Nginx for Nextcloud **" &&
-    echo "extension="smbclient.so"" >php-fpm/conf.d/00_smbclient.ini &&
-    echo 'apc.enable_cli=1' >>php-fpm/conf.d/apcu.ini &&
-    sed -i \
-        -e 's/;opcache.enable.*=.*/opcache.enable=1/g' \
-        -e 's/;opcache.interned_strings_buffer.*=.*/opcache.interned_strings_buffer=16/g' \
-        -e 's/;opcache.max_accelerated_files.*=.*/opcache.max_accelerated_files=10000/g' \
-        -e 's/;opcache.memory_consumption.*=.*/opcache.memory_consumption=128/g' \
-        -e 's/;opcache.save_comments.*=.*/opcache.save_comments=1/g' \
-        -e 's/;opcache.revalidate_freq.*=.*/opcache.revalidate_freq=1/g' \
-        -e 's/;always_populate_raw_post_data.*=.*/always_populate_raw_post_data=-1/g' \
-        -e 's/memory_limit.*=.*128M/memory_limit=512M/g' \
-        -e 's/max_execution_time.*=.*30/max_execution_time=120/g' \
-        -e 's/upload_max_filesize.*=.*2M/upload_max_filesize=1024M/g' \
-        -e 's/post_max_size.*=.*8M/post_max_size=1024M/g' \
-        -e 's/output_buffering.*=.*/output_buffering=0/g' \
-        php-fpm/php.ini &&
-    sed -i \
-        '/opcache.enable=1/a opcache.enable_cli=1' \
-        php-fpm/php.ini &&
-    echo "env[PATH] = /usr/local/bin:/usr/bin:/bin" >>php-fpm/php-fpm.conf
 touch ./logs/installed
+rm -rf /tmp/*
+echo "**** configure php and nginx for nextcloud ****" && \
+echo "extension="smbclient.so"" > php-fpm/conf.d/00_smbclient.ini && \
+echo 'apc.enable_cli=1' >> php-fpm/conf.d/apcu.ini && \
+sed -i \
+-e 's/;opcache.enable.*=.*/opcache.enable=1/g' \
+-e 's/;opcache.interned_strings_buffer.*=.*/opcache.interned_strings_buffer=16/g' \
+-e 's/;opcache.max_accelerated_files.*=.*/opcache.max_accelerated_files=10000/g' \
+-e 's/;opcache.memory_consumption.*=.*/opcache.memory_consumption=128/g' \
+-e 's/;opcache.save_comments.*=.*/opcache.save_comments=1/g' \
+-e 's/;opcache.revalidate_freq.*=.*/opcache.revalidate_freq=1/g' \
+-e 's/;always_populate_raw_post_data.*=.*/always_populate_raw_post_data=-1/g' \
+-e 's/memory_limit.*=.*128M/memory_limit=512M/g' \
+-e 's/max_execution_time.*=.*30/max_execution_time=120/g' \
+-e 's/upload_max_filesize.*=.*2M/upload_max_filesize=1024M/g' \
+-e 's/post_max_size.*=.*8M/post_max_size=1024M/g' \
+-e 's/output_buffering.*=.*/output_buffering=0/g' \
+php-fpm/php.ini && \
+sed -i \
+'/opcache.enable=1/a opcache.enable_cli=1' \
+php-fpm/php.ini && \
+echo "env[PATH] = /usr/local/bin:/usr/bin:/bin" >> php-fpm/php-fpm.conf
 mkdir tmp
