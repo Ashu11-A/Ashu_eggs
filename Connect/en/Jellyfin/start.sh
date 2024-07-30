391003d23dc2058918d07cf2f08af8a11c193a0e
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-07-29 21:11:27 -0400

    🐛 Jellyfin: Fixing permission problem on some machines

--- Connect/en/Jellyfin/start.sh
+++ Connect/en/Jellyfin/start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo "⚙️ Script Version: 2.1"
+echo "⚙️ Script Version: 2.2"
 if [[ -f "./jellyfin/jellyfin.dll" ]]; then
+	echo "🔏  Setting file permissions"
+	chmod 777 -R ./*
     echo "🟢  Starting Nginx..."
     nohup /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/ 2>&1 &
 
-    echo "✅ Starting Jellyfin"
+    echo "✅  Starting Jellyfin"
     dotnet jellyfin/jellyfin.dll --ffmpeg /usr/lib/jellyfin-ffmpeg/ffmpeg
 else
     echo "Jellyfin Not Installed, this is really odd, this is a second verification."
--- Connect/pt-BR/Jellyfin/start.sh
+++ Connect/pt-BR/Jellyfin/start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo "⚙️  Versão do Script: 2.0"
+echo "⚙️ Script Version: 2.2"
 if [[ -f "./jellyfin/jellyfin.dll" ]]; then
+	echo "🔏  Definindo as permissões dos arquivos"
+	chmod 777 -R ./*
     echo "🟢  Iniciando Nginx..."
     nohup /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/ 2>&1 &
 
-    echo "✅ Iniciando Jellyfin"
+    echo "✅  Iniciando Jellyfin"
     dotnet jellyfin/jellyfin.dll --ffmpeg /usr/lib/jellyfin-ffmpeg/ffmpeg
 else
     echo "Jellyfin Não Instalado, isso é realmente muito estranho, essa é uma segunda verificação."
