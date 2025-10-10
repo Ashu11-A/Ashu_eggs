22b9940a855fc94e7f1e512bfcbe80b4abde41eb
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2025-10-10 19:48:17 -0300

    ✅ Nextcloud recommended implementations

--- Connect/all/Nextcloud/launch.sh
+++ Connect/all/Nextcloud/launch.sh
@@ -18,13 +18,48 @@ if [[ ! -f "./logs/installed" ]]; then
   exit
 fi
 
+# Limpa diretório temporário
 rm -rf /home/container/tmp/*
 
+# Permite executar comandos OCC diretamente
 if [[ $OCC == "1" ]]; then 
   php ./nextcloud/occ ${COMMANDO_OCC}
   exit
 else
   echo "⚙️ Script Version: 2.0"
+
+  # Define as configurações de cache usando o comando occ para segurança e idempotência.
+  echo "🛠 Configuring cache (APCu, Memcached, Redis)"
+  php ./nextcloud/occ config:system:set memcache.local --value='\OC\Memcache\APCu'
+  php ./nextcloud/occ config:system:set memcache.distributed --value='\OC\Memcache\Memcached'
+  php ./nextcloud/occ config:system:set memcache.locking --value='\OC\Memcache\Redis'
+  
+  # Adiciona os servidores Memcached. O valor é passado como uma string JSON.
+  php ./nextcloud/occ config:system:set memcached_servers --value='[["localhost", 11211]]' --type=json
+  
+  # Configura os detalhes de conexão do Redis
+  php ./nextcloud/occ config:system:set redis host --value='localhost'
+  php ./nextcloud/occ config:system:set redis port --value='6379' --type=integer
+
+  echo "🛠 Configuring upload limits (16GB)"
+  echo "🛠 Aplicando configurações de upload em nextcloud/.user.ini..."
+  cat > ./nextcloud/.user.ini << EOL
+always_populate_raw_post_data=-1
+default_charset='UTF-8'
+output_buffering=0
+
+php_value upload_max_filesize 16G
+php_value post_max_size 16G
+php_value max_input_time 3600
+php_value max_execution_time 3600
+EOL
+
+  echo "🛠 Starting Redis Server..."
+  redis-server --daemonize yes
+
+  echo "🛠 Starting Memcached..."
+  memcached -d -u nobody
+
   echo "🛠 Starting PHP-FPM..."
   /usr/sbin/php-fpm --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
