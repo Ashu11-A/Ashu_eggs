6d1445eff89ed60faeaf8d7e47d1dbb707bdc900
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2025-10-12 21:10:28 -0300

    🐛 Fix: conditional binary operator expected

--- Connect/all/Frp/start.sh
+++ Connect/all/Frp/start.sh
@@ -3,7 +3,7 @@
 export LANG_PATH="https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Lang/frp.conf"
 curl -sSL https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Utils/loadLang.sh -o loadLang.sh
 
-if [[ ! -f "./Frps/frps" || !-f "./Frpc/frpc" ]]; then
+if [[ ! -f "./Frps/frps" || ! -f "./Frpc/frpc" ]]; then
   echo "en" > logs/language.conf
 
   source ./loadLang.sh
@@ -16,6 +16,8 @@ if [[ ! -f "./Frps/frps" || !-f "./Frpc/frpc" ]]; then
   rm ./logs/language.conf
   exit
 fi
+
+# update please
 
 source ./loadLang.sh
 set -a
