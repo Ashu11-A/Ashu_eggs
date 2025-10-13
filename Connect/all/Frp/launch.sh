fdf6cf95007a6935652f03306b04d325c337cabb
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2025-10-12 23:43:44 -0300

    ✨

--- Connect/all/Frp/launch.sh
+++ Connect/all/Frp/launch.sh
@@ -16,12 +16,25 @@ if [ "${FRP_MODE}" == "1" ]; then
 
   if [[ -f "./Frps/frps.toml" ]]; then
     # Configura os valores do frps.toml
+    printf "$configuring_bind_port\n" "${BIND_PORT}"
     set_toml_value "Frps/frps.toml" "bindPort" "${BIND_PORT}"
+    
+    printf "$configuring_kcp_port\n" "${BIND_PORT}"
     set_toml_value "Frps/frps.toml" "kcpBindPort" "${BIND_PORT}"
+    
+    printf "$configuring_webserver_addr\n" "0.0.0.0"
     set_toml_value "Frps/frps.toml" "webServer.addr" "0.0.0.0"
+    
+    printf "$configuring_webserver_port\n" "${DASHBOARD_PORT}"
     set_toml_value "Frps/frps.toml" "webServer.port" "${DASHBOARD_PORT}"
+    
+    printf "$configuring_webserver_user\n" "${DASHBOARD_USER}"
     set_toml_value "Frps/frps.toml" "webServer.user" "${DASHBOARD_USER}"
+    
+    echo "$configuring_webserver_password"
     set_toml_value "Frps/frps.toml" "webServer.password" "${DASHBOARD_PWD}"
+    
+    echo "$configuring_auth_token"
     set_toml_value "Frps/frps.toml" "auth.token" "${TOKEN}"
     
     ./Frps/frps -c ./Frps/frps.toml
@@ -34,8 +47,13 @@ else
 
   if [[ -f "./Frpc/frpc.toml" ]]; then
     # Configura os valores do frpc.toml
+    printf "$configuring_server_port\n" "${BIND_PORT}"
     set_toml_value "Frpc/frpc.toml" "serverPort" "${BIND_PORT}"
+    
+    printf "$configuring_auth_method\n" "token"
     set_toml_value "Frpc/frpc.toml" "auth.method" "token"
+    
+    echo "$configuring_auth_token"
     set_toml_value "Frpc/frpc.toml" "auth.token" "${TOKEN}"
     
     ./Frpc/frpc -c ./Frpc/frpc.toml
