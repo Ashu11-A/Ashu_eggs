7057d3cfcb4ec30d50ded115e98e1fb4041ce7f5
Matheus Nilton Biolowons <30575805+Ashu11-A@users.noreply.github.com>
2025-11-19 10:33:26 -0300

    🔓 add chmod +x

--- Connect/all/Paneldactyl/start.sh
+++ Connect/all/Paneldactyl/start.sh
@@ -4,7 +4,9 @@ mkdir -p logs
 
 rm -f start.sh
 curl -sSL https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Connect/all/Paneldactyl/start.sh -o start.sh
+chmod +x start.sh
 
 curl -sSL https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Connect/all/Paneldactyl/install.sh -o /tmp/install.sh
+chmod +x /tmp/install.sh
 bash /tmp/install.sh | tee logs/terminal.log
 rm -f /tmp/install.sh
