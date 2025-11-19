c5a8745e16e22742b15343470b076063927994b0
Matheus Nilton Biolowons <30575805+Ashu11-A@users.noreply.github.com>
2025-11-19 09:49:09 -0300

    ♾️ Always update the scripts

--- Connect/all/Paneldactyl/start.sh
+++ Connect/all/Paneldactyl/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-curl -sSL https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Connect/all/Paneldactyl/install.sh -o /tmp/install_redirect.sh
-bash /tmp/install_redirect.sh | tee logs/terminal.log
-rm -f /tmp/install_redirect.sh
+mkdir -p logs
+
+rm -f start.sh
+curl -sSL https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Connect/all/Paneldactyl/start.sh -o start.sh
+
+curl -sSL https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Connect/all/Paneldactyl/install.sh -o /tmp/install.sh
+bash /tmp/install.sh | tee logs/terminal.log
+rm -f /tmp/install.sh
