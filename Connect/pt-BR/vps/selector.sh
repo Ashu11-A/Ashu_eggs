e743151d864097051015ef92a21222fb0409f9cd
Matheus Nilton Biolowons <30575805+Ashu11-A@users.noreply.github.com>
2026-02-24 04:56:48 -0300

    🚮 This VPS is garbage lol

--- Connect/pt-BR/vps/selector.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ARCH=$([ "$(uname -m)" == "x86_64" ] && echo "amd64" || echo "arm64")
-
-if [ "${ARCH}" == "arm64" ];
-then
-bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Connect/pt-BR/vps/ARM64/installer.sh)
-else 
-bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Connect/pt-BR/vps/64x/installer.sh)
-fi
