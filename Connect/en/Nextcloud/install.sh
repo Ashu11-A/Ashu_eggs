00e0640ee5a111a6e7d3cc5f5f404f55bd47db5b
Matheus Nilton Biolowons <30575805+Ashu11-A@users.noreply.github.com>
2026-02-26 00:06:06 -0300

    >_ Use Bash

--- Connect/en/Nextcloud/install.sh
+++ Connect/en/Nextcloud/install.sh
@@ -1,9 +1,13 @@
 #!/bin/ash
 
+if [ ! -d "/home/container" ]; then
+    apk add bash
+fi
+
 if [ -f "./launch.sh" ]; then
   rm ./launch.sh
 fi
 
 curl -sO https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/Connect/all/Nextcloud/launch.sh
 chmod +x launch.sh
-./launch.sh
+bash ./launch.sh
